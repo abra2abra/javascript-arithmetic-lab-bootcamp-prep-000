d27db7a46c26972eee029b6f5a7a3b34fc82e99c
abra2abra <abra2abra@mailinator.com>
2018-03-08 23:53:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -40,3 +40,15 @@ function divide(a,b){
 function inc(n){
   return n++;
 }
+
+function dec(n){
+  return n--;
+}
+
+function makeInt(n){
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n){
+  return parseFloat(n);
+}
