5db572733384aed7615a51b1432123b8305501fc
abra2abra <abra2abra@mailinator.com>
2018-03-08 23:41:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var number = 10;
 
-function add(){
-  return 
+function add5(){
+  return number += 5;
 }
-function subtract(){}
-function multiply(){}
-function divide(){}
+//function subtract(){}
+//function multiply(){}
+function divideBy3(){
+  return number /= 3;
+}
+
+divideBy3();
+console.log(number);
+
