85a0c9ba57526a572fe81c3f8f2647bfd43d3411
abra2abra <abra2abra@mailinator.com>
2018-03-08 23:50:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,6 +25,14 @@ function add(a,b){
   return a + b;
 }
 
-function substract(a, b){
+function subtract(a, b){
   return b-a;
+}
+
+function multiply(a,b){
+  return a*b;
+}
+
+function divide(a,b){
+  return a
 }
